fc410c4f51526034873916eecc21f66b80d6aa7a
Keisuke <inokei1704@gmail.com>
2024-07-20 19:31:15 +0900

    レイアウトを変更

--- src/components/SalmonRun.jsx
+++ src/components/SalmonRun.jsx
@@ -4,30 +4,25 @@ import styles from 'styles/Home.module.css';
 export default function SRunSchedule({ salmonSch }) {
     return (
         <div className={styles.schedules}>
-            {(() => {
-                return salmonSch.results.map((item, index) => {
-                    return (
-                        <div key={index} className={styles.schedule}>
-                            <p className={styles.time}>{formatDate(item.start_time, item.end_time)}</p>
-                            <li className={styles.stageList} key={item.stage.id}>
-                                <div className={styles.stageAndBoss}>
-                                    <p className={styles.stageName}>{item.stage.name}</p>
-                                    <p className={styles.bossName}>{item.boss.name}</p>
-                                </div>
-                                <img className={styles.stageImage} src={item.stage.image} alt={item.stage.name} />
-                            </li>
-                            <ul className={styles.weaponSetList}>
-                                {item.weapons.map(weapon => (
-                                    <li className={styles.weaponList}>
-                                        {/* <p className={styles.weaponName}>{weapon.name}</p> */}
-                                        <img className={styles.weaponImage} src={weapon.image} alt={weapon.image}></img>
-                                    </li>
-                                ))}
-                            </ul>
+            {salmonSch.results.map((item, index) => (
+                <div key={index} className={styles.schedule}>
+                    <p className={styles.time}>{formatDate(item.start_time, item.end_time)}</p>
+                    <div className={styles.stageList}>
+                        <div className={styles.stageAndBoss}>
+                            <p className={styles.stageName}>{item.stage.name}</p>
+                            <p className={styles.bossName}>{item.boss.name}</p>
                         </div>
-                    );
-                });
-            })()}
+                        <img className={styles.stageImage} src={item.stage.image} alt={item.stage.name} />
+                    </div>
+                    <ul className={styles.weaponSetList}>
+                        {item.weapons.map((weapon, weaponIndex) => (
+                            <li key={weaponIndex} className={styles.weaponList}>
+                                <img className={styles.weaponImage} src={weapon.image} alt={weapon.name} />
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            ))}
         </div>
-    )
+    );
 }
