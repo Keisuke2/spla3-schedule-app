49684bb0af2db1f3fb77cbce056064452bb32975
Keisuke <inokei1704@gmail.com>
2024-07-27 20:21:49 +0900

    各バトルを識別するコンテナを追加

--- src/components/X.jsx
+++ src/components/X.jsx
@@ -15,29 +15,31 @@ export default function XSchedule({ sch, maxDisplayedItems }) {
     const isXSchAvailable = filteredXSch.every(item => item.is_fest);
 
     return (
-        <div className={styles.scheduleContainer}>
-            {isXSchAvailable ? (
-                <p className={styles.noSchMsg}>フェス開催中のため現在スケジュールはありません</p>
-            ) : (
-                filteredXSch.map((item, index) => (
-                    !item.is_fest && (
-                        <div key={index} className={styles.scheduleItem}>
-                            <div className={styles.scheduleInfo}>
-                                <p className={styles.time}>{formatDate(item.start_time, item.end_time)}</p>
-                                <p className={styles.rule}>{item.rule.name}</p>
+        <div className={styles.xContainer}>
+            <div className={styles.scheduleContainer}>
+                {isXSchAvailable ? (
+                    <p className={styles.noSchMsg}>フェス開催中のため現在スケジュールはありません</p>
+                ) : (
+                    filteredXSch.map((item, index) => (
+                        !item.is_fest && (
+                            <div key={index} className={styles.scheduleItem}>
+                                <div className={styles.scheduleInfo}>
+                                    <p className={styles.time}>{formatDate(item.start_time, item.end_time)}</p>
+                                    <p className={styles.rule}>{item.rule.name}</p>
+                                </div>
+                                <div className={styles.stageContainer}>
+                                    {item.stages.map(stage => (
+                                        <div key={stage.id} className={styles.stageItem}>
+                                            <img className={styles.stageImage} src={stage.image} alt={stage.name} />
+                                            <p className={styles.stageName}>{stage.name}</p>
+                                        </div>
+                                    ))}
+                                </div>
                             </div>
-                            <div className={styles.stageContainer}>
-                                {item.stages.map(stage => (
-                                    <div key={stage.id} className={styles.stageItem}>
-                                        <img className={styles.stageImage} src={stage.image} alt={stage.name} />
-                                        <p className={styles.stageName}>{stage.name}</p>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    )
-                ))
-            )}
+                        )
+                    ))
+                )}
+            </div>
         </div>
     );
 }
