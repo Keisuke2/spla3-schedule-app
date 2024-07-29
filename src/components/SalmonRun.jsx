ef403907b1465bd6e0b40937439c60ed017b1165
Keisuke <inokei1704@gmail.com>
2024-07-29 18:00:15 +0900

    レスポンシブデザイン・レイアウトの変更

--- src/components/SalmonRun.jsx
+++ src/components/SalmonRun.jsx
@@ -19,14 +19,14 @@ export default function SRunSchedule({ salmonSch }) {
                                 <img className={styles.stageImage} src={item.stage.image} alt={item.stage.name} />
                                 <p className={styles.stageName}>{item.stage.name}</p>
                             </div>
-                            <div className={styles.weaponContainer}>
-                                {item.weapons.map((weapon, weaponIndex) => (
-                                    <div key={weaponIndex} className={styles.weaponItem}>
-                                        <img className={styles.weaponImage} src={weapon.image} alt={weapon.name} />
-                                        <div className={styles.weaponName}>{weapon.name}</div>
-                                    </div>
-                                ))}
-                            </div>
+                        </div>
+                        <div className={styles.weaponContainer}>
+                            {item.weapons.map((weapon, weaponIndex) => (
+                                <div key={weaponIndex} className={styles.weaponItem}>
+                                    <img className={styles.weaponImage} src={weapon.image} alt={weapon.name} />
+                                    <div className={styles.weaponName}>{weapon.name}</div>
+                                </div>
+                            ))}
                         </div>
                     </div>
                 ))}
