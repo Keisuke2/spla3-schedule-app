87e858559abde736a9e28769c26f0d17649c6ea3
Keisuke <inokei1704@gmail.com>
2024-07-31 01:02:13 +0900

    ボタン部分のcssを独立、ボタン画像を有効にする

--- src/components/tabs/Button.jsx
+++ src/components/tabs/Button.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { TabList, Tab } from '@headlessui/react';
-import styles from 'styles/Home.module.css';
+import styles from 'styles/Tab.module.css';
 
 const TabButton = ({ src, alt, children, isActive, onClick }) => (
     <Tab className={`${styles.tab} ${isActive ? styles.active : ''}`} onClick={onClick}>
@@ -24,43 +24,43 @@ const TabButtons = () => {
     return (
         <TabList className={styles.tabs}>
             <TabButton
-                // src='/images/button/regular.png'
-                // alt='レギュラーマッチのアイコン'
+                src='/images/button/regular.png'
+                alt='レギュラーマッチのアイコン'
                 isActive={activeTab === 0}
                 onClick={() => setActiveTab(0)}
             > <p className={styles.tabName}>レギュラーマッチ</p>
             </TabButton>
             <TabButton
-                // src='/images/button/bankara.png'
-                // alt='バンカラマッチのアイコン'
+                src='/images/button/bankara.png'
+                alt='バンカラマッチのアイコン'
                 isActive={activeTab === 1}
                 onClick={() => setActiveTab(1)}
             > <p className={styles.tabName}>バンカラマッチ</p>
             </TabButton>
             <TabButton
-                // src='/images/button/x.png'
-                // alt='Xマッチのアイコン'
+                src='/images/button/x.png'
+                alt='Xマッチのアイコン'
                 isActive={activeTab === 2}
                 onClick={() => setActiveTab(2)}
             > <p className={styles.tabName}>Xマッチ</p>
             </TabButton>
             <TabButton
-                // src='/images/button/event.png'
-                // alt='イベントマッチのアイコン'
+                src='/images/button/event.png'
+                alt='イベントマッチのアイコン'
                 isActive={activeTab === 3}
                 onClick={() => setActiveTab(3)}
             > <p className={styles.tabName}>イベントマッチ</p>
             </TabButton>
             <TabButton
-                // src='/images/button/fest.png'
-                // alt='フェスマッチのアイコン'
+                src='/images/button/fest.png'
+                alt='フェスマッチのアイコン'
                 isActive={activeTab === 4}
                 onClick={() => setActiveTab(4)}
             > <p className={styles.tabName}>フェスマッチ</p>
             </TabButton>
             <TabButton
-                // src='/images/button/salmonrun.png'
-                // alt='サーモンランのアイコン'
+                src='/images/button/salmonrun.png'
+                alt='サーモンランのアイコン'
                 isActive={activeTab === 5}
                 onClick={() => setActiveTab(5)}
             > <p className={styles.tabName}>サーモンラン</p>
