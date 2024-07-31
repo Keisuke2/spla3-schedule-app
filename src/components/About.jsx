a365337d71d1e01ab44353ec629c33d6e0f94781
Keisuke <inokei1704@gmail.com>
2024-07-31 18:56:25 +0900

    Aboutページをコンポーネント化し、レイアウトを修正

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import Head from "next/head";
 import styles from "../styles/About.module.css";
 
 export default function About() {
+    return (
+        <>
+            <Header />
+            <Main />
+        </>
+    );
+}
+
+function Header() {
+    return (
+        <Head prefix='og:http://ogp.me/ns#'>
+            <meta charSet='utf-8' />
+            <meta name='viewport' content='width=device-width' />
+            <title>Splatoon3 最新スケジュール</title>
+            <meta name="description" content="このアプリについて スプラトゥーン3の最新スケジュール" />
+            <meta property="og:url" content="https://www.splatoon3-schedule.net/" />
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content="Splatoon3 スケジュール" />
+            <meta property="og:description" content="このアプリについて スプラトゥーン3の最新スケジュール" />
+            <meta property="og:site_name" content="Splatoon3 スケジュール" />
+            <meta name="twitter:card" content="summary_large_image" />
+            <link rel="manifest" href="/manifest.json" />
+            <link rel="apple-touch-icon" href="/icon.png"></link>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+    )
+}
+
+function Main() {
     return (
         <div className={styles.container}>
             <p className={styles.lastUpdated}>最終更新日: 2023年7月30日</p>
