baaf9fe1a65ad922b1b9529ba5f2294542de585f
Keisuke <inokei1704@gmail.com>
2024-08-13 15:45:43 +0900

    aboutページの修正と追加

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Head from "next/head";
+import Script from 'next/script';
 import styles from "../styles/About.module.css";
 
 export default function About() {
@@ -12,46 +13,45 @@ export default function About() {
 }
 
 function Header() {
-    useEffect(() => {
-        const script = document.createElement('script');
-        script.innerHTML = `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-MS3PGCXS');
-        `;
-        document.head.appendChild(script);
-
-        return () => {
-            document.head.removeChild(script);
-        };
-    }, []);
 
     return (
-        <Head prefix='og:http://ogp.me/ns#'>
-
-            <meta charSet='utf-8' />
-            <meta name='viewport' content='width=device-width' />
-            <title>Splatoon3 最新スケジュール</title>
-            <meta name="description" content="このアプリについて スプラトゥーン3の最新スケジュール" />
-            <meta property="og:url" content="https://www.splatoon3-schedule.net/" />
-            <meta property="og:type" content="website" />
-            <meta property="og:title" content="Splatoon3 スケジュール" />
-            <meta property="og:description" content="このアプリについて スプラトゥーン3の最新スケジュール" />
-            <meta property="og:site_name" content="Splatoon3 スケジュール" />
-            <meta name="twitter:card" content="summary_large_image" />
-            <link rel="manifest" href="/manifest.json" />
-            <link rel="apple-touch-icon" href="/icon.png"></link>
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
+        <>
+            <Head prefix='og:http://ogp.me/ns#'>
+                <meta charSet='utf-8' />
+                <meta name='viewport' content='width=device-width' />
+                <title>Splatoon3 最新スケジュール</title>
+                <meta name="description" content="このアプリについて スプラトゥーン3の最新スケジュール" />
+                <meta property="og:url" content="https://www.splatoon3-schedule.net/" />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="Splatoon3 スケジュール" />
+                <meta property="og:description" content="このアプリについて スプラトゥーン3の最新スケジュール" />
+                <meta property="og:site_name" content="Splatoon3 スケジュール" />
+                <meta name="twitter:card" content="summary_large_image" />
+                <link rel="manifest" href="/manifest.json" />
+                <link rel="apple-touch-icon" href="/icon.png"></link>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Script
+                id="gtm-script"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-MS3PGCXS');
+                    `
+                }}
+            />
+        </>
     )
 }
 
 function Main() {
     return (
         <div className={styles.container}>
-            <p className={styles.lastUpdated}>最終更新日: 2023年7月30日</p>
+            <p className={styles.lastUpdated}>最終更新日: 2024年8月13日</p>
             <h1 className={styles.title}>About</h1>
 
             <section className={styles.section}>
@@ -61,14 +61,19 @@ function Main() {
                 <p className={styles.paragraph}>スマートフォンやタブレット、PCからアクセスできます。</p>
             </section>
 
+            <section>
+                <h2 className={styles.sectionTitle}>今後のアップデートについて</h2>
+                <ul>
+                    <li className={styles.listItem}>ビッグランのスケジュール追加予定</li>
+                    <li className={styles.listItem}>バイトチームコンテストのスケジュール追加予定</li>
+                    <li className={styles.listItem}>見やすいレイアウトに修正予定</li>
+                </ul>
+            </section>
+
             <section className={styles.section}>
                 <h2 className={styles.sectionTitle}>使用リソース</h2>
                 <p className={styles.paragraph}>スケジュールの取得にあたり使用しているAPIについては<a href="https://spla3.yuu26.com/" className={styles.link}>こちら</a>をクリック。</p>
                 <p className={styles.paragraph}>使用しているfaviconについては<a href="https://onwa-illust.com/material/26189/" className={styles.link}>こちら</a>をクリック。</p>
-            </section>
-
-            <section className={styles.section}>
-                <h2 className={styles.sectionTitle}>アイコンについて</h2>
                 <p className={styles.paragraph}>スケジュールの選択ボタンで使用しているアイコンについては以下をクリック。</p>
                 <ul className={styles.list}>
                     <li className={styles.listItem}><a href="https://splatoonwiki.org/wiki/Regular_Battle" className={styles.link}>レギュラーマッチのアイコン</a></li>
