8723f0d34f674b00d07183bfa1707262924d2bb0
Keisuke <inokei1704@gmail.com>
2024-08-13 15:02:46 +0900

    Google タグマネージャの実装

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,38 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Head from 'next/head';
+import Script from 'next/script';
 
 export default function Header() {
-    useEffect(() => {
-        const script = document.createElement('script');
-        script.innerHTML = `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-MS3PGCXS');
-        `;
-        document.head.appendChild(script);
-
-        return () => {
-            document.head.removeChild(script);
-        };
-    }, []);
-
     return (
-        <Head prefix='og:http://ogp.me/ns#'>
-            <meta charSet='utf-8' />
-            <meta name='viewport' content='width=device-width' />
-            <title>Splatoon3 最新スケジュール</title>
-            <meta name="description" content="スプラトゥーン3の最新スケジュール（レギュラー、バンカラ、X、イベント、フェス、サーモンラン）を見るならここ!!" />
-            <meta property="og:url" content="https://www.splatoon3-schedule.net/" />
-            <meta property="og:type" content="website" />
-            <meta property="og:title" content="Splatoon3 スケジュール" />
-            <meta property="og:description" content="スプラトゥーン3の最新スケジュール（レギュラー、バンカラ、X、イベント、フェス、サーモンラン）を見るならここ!!" />
-            <meta property="og:site_name" content="Splatoon3 スケジュール" />
-            <meta name="twitter:card" content="summary_large_image" />
-            <link rel="manifest" href="/manifest.json" />
-            <link rel="apple-touch-icon" href="/icon.png"></link>
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
+        <>
+            <Head prefix='og:http://ogp.me/ns#'>
+                <meta charSet='utf-8' />
+                <meta name='viewport' content='width=device-width' />
+                <title>Splatoon3 最新スケジュール</title>
+                <meta name="description" content="スプラトゥーン3の最新スケジュール（レギュラー、バンカラ、X、イベント、フェス、サーモンラン）を見るならここ!!" />
+                <meta property="og:url" content="https://www.splatoon3-schedule.net/" />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="Splatoon3 スケジュール" />
+                <meta property="og:description" content="スプラトゥーン3の最新スケジュール（レギュラー、バンカラ、X、イベント、フェス、サーモンラン）を見るならここ!!" />
+                <meta property="og:site_name" content="Splatoon3 スケジュール" />
+                <meta name="twitter:card" content="summary_large_image" />
+                <link rel="manifest" href="/manifest.json" />
+                <link rel="apple-touch-icon" href="/icon.png" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Script
+                id="gtm-script"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-MS3PGCXS');
+                    `
+                }}
+            />
+        </>
     )
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,23 +1,20 @@
 import React from 'react'
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 
-class GoogleTagManager extends Document {
-    render() {
-        return (
-            <Html>
-                <Head />
-                <body>
-                    <noscript>
-                        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MS3PGCXS"
-                            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}>
-                        </iframe>
-                    </noscript>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+export default function Document() {
+    return (
+        <Html>
+            <Head />
+            <body>
+                {/* Google Tag Manager (noscript) */}
+                <noscript>
+                    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MS3PGCXS"
+                        height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
+                </noscript>
+                {/* End Google Tag Manager (noscript) */}
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
 }
-
-export default GoogleTagManager
