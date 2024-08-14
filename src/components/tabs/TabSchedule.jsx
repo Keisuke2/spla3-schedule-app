a9c527dc2bf9ff8c08add924925f2400099e4b0a
Keisuke <inokei1704@gmail.com>
2024-08-14 16:16:35 +0900

    ボタンバーのレイアウトを修正

--- src/components/tabs/TabSchedule.jsx
+++ src/components/tabs/TabSchedule.jsx
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types';
 import { TabGroup } from '@headlessui/react';
 import TabContents from 'components/tabs/Content';
 import TabButtons from 'components/tabs/Button';
+import styles from 'styles/Tab.module.css';
 
 const TabSchedule = ({ sch, festOpSch, salmonSch }) => {
     return (
-        <TabGroup>
+        <TabGroup className={styles.tabsContainer}>
             <TabButtons sch={sch} festOpSch={festOpSch} salmonSch={salmonSch} />
             <TabContents sch={sch} festOpSch={festOpSch} salmonSch={salmonSch} />
         </TabGroup>
